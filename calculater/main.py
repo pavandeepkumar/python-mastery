71f2938f8fe34aff2847eec6e4d034846e74bc56
Pavandeep <pavandeepkumarmlk@gmail.com>
2024-01-12 19:07:36 +0530

    basic of python

---
+++ calculater/main.py
@@ -0,0 +1,169 @@
+
+# firstNumber=input("Enter Your firstNumber:  ")
+# opera=input("Enter Your Operater:  ")
+# secondNumber=input("Enter Your firstNumber:  ")
+# print("operater",firstNumber)
+# print("operater",type(firstNumber))
+
+
+# number=int(firstNumber)
+
+# print("firstNumber",type(number))
+
+
+
+
+
+# # # print("Your result of ",int(firstNumber))
+# age=input("What is your Age :  ")
+# userAge=int(age)
+# if (userAge>18):
+#   print("You are Now able to vote ")
+# elif(userAge<18):
+#   print("You are Now not able to vote")
+# else:
+#   print("Error")
+
+
+# how to declear function in python
+# def add_numbers(a,b):
+
+
+
+# x = None
+
+# #display x:
+# print(x)
+
+# #display the data type of x:
+# print(type(x)) 
+
+# tuple example
+# x = {"name" : "John", "age" : 36,"subject":"math",}
+
+# #display x:
+# print(x)
+
+# #display the data type of x:
+# print(type(x)) 
+
+
+# # list
+
+# student=["pavandeep",12,"B.Tech"]
+
+# print(student)
+# print(len(student))
+# print(type(student))
+
+
+# x = ("apple", "banana", "cherry")
+
+# #display x:
+# print(x)
+
+# #display the data type of x:
+# print(type(x)) 
+
+# x={"name":"pavandeep","age":19}
+
+
+# print(x)
+# print(type(x))
+
+
+# x=["name","age"]
+
+
+# print(type(x))
+# print(x)
+
+# x=("name","age")
+
+# print(x)
+
+# print(type(x))
+
+
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# #convert from int to float:
+# a = float(x)
+
+# #convert from float to int:
+# b = int(y)
+
+# #convert from int to complex:
+# c = complex(x)
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+# import random
+
+# randomNumber=random.randrange(1,10)
+
+# otp=randomNumber*10000
+
+# print(random.randrange(10000000, 12222222))
+# print(otp)
+
+
+# for x in "banana":
+#   print(x) 
+
+# a = "Hello, World!"
+# print(len(a))
+
+
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
+
+
+
+# txt = "The best things in life are free!"
+# print("expensive" not in txt)
+
+
+# txt = "The best things in life are free!"
+# if "expensive" not in txt:
+#   print("No, 'expensive' is NOT present.")
+
+
+# b = "Hello, World!"
+# print(b[2:5])
+
+
+# def myFunction(a,b) :
+#   return int(a)+int(b)
+
+# print(myFunction(2,3))
+
+
+# def add(a,b):
+#     return int(a)+int(b)
+
+
+
+# print(add(2,4))
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[0]="pavandeep"
+# print(len(thislist))
+# print(thislist[0:3])
+
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
